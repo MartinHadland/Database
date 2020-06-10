026f5be870296fb298f79512a541ebd3a20e3a48
Martin27 <66727215+MartinHadland@users.noreply.github.com>
2020-06-10 15:37:39 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserEntry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("What is your Surname?");
+            string surname = Console.ReadLine();
+            Console.WriteLine("What is your age?");
+            string age = Console.ReadLine();
+
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Surname: " + surname);
+            Console.WriteLine("Age: " + age);
+
+            Console.Read();
+        }
+    }
+}
